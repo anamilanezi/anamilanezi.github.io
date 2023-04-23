e6ba2bea14b88defa7507d56f3a9db7ccff7ab26
Ana Milanezi <anamilanezi@gmail.com>
2023-04-23 19:24:43 -0300

    hide projects until fix

--- script/script.js
+++ script/script.js
@@ -1,7 +1,7 @@
 const panels = document.querySelectorAll('.panel')
 
 panels.forEach((panel) => {
-    panel.addEventListener('mouseover', () => {
+    panel.addEventListener('click', () => {
         removeActiveClasses();
         panel.classList.add('active');
         console.log('clicked')
@@ -19,3 +19,61 @@ const resume = document.querySelector('.resume__title')
 resume.addEventListener('mouseover', () => {
     resume.classList.remove('fa-fade')
 })
+
+const panelUrl = (projectName) => {
+    return `https://anamilanezi.github.io/front-end-mentor/${projectName}/index.html`
+}
+
+
+// const clicks = {
+//     card: 0,
+//     tip: 0,
+//     faq: 0,
+//     rating: 0,
+//     capstone: 0,
+//     pomodoge: 0,
+
+// }
+// const projectCard = document.querySelector('.panel.card')
+
+
+// const openProject = (clickedPanel, projectClassName, projectLink) => {
+//     clickedPanel.addEventListener('click', (e) => {
+//         e.stopPropagation()
+//         if (clicks[projectClassName] >= 2 ) {
+//             if (clickedPanel.classList.contains(projectClassName)) {
+//                 window.open(panelUrl(projectLink))
+//             }
+//         }
+//     })
+//     console.log(clicks)
+//     clicks[projectClassName] = 1;
+// }
+
+
+
+
+
+// openProject(projectCard, 'card', 'interactive-card-details') 
+
+
+// projectCard.addEventListener('click', () => {
+//     if (cardClick >= 1) {
+//         if (projectCard.classList.contains('card')) {
+//             console.log(projectCard.classList.contains('card'))
+//             window.open(panelUrl('interactive-card-details'), '_blank');
+//         }
+//     } 
+//     cardClick++;
+// })
+
+
+// projectTip.addEventListener('click', () => {
+//     if (clicks.tip >= 1) {
+//         if (projectCard.classList.contains('card')) {
+//             console.log(projectCard.classList.contains('card'))
+//             window.open(panelUrl('interactive-card-details'), '_blank');
+//         }
+//     } 
+//     clicks.tip++;
+// })
